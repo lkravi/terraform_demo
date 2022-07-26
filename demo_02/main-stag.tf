babe09b9eed55f1e25b46ac602f3f19a2c3c0874
Ravi Yasakeerthi <sliit.rnp@gmail.com>
2022-07-26 13:16:57 +1000

    addming demo 2

---
+++ demo_02/main-stag.tf
@@ -0,0 +1,26 @@
+## Test VPC
+resource "aws_vpc" "test" {
+  cidr_block            = "10.0.0.0/16"
+  enable_dns_support    = true
+  enable_dns_hostnames  = true 
+}
+
+## Test public Subnet
+resource "aws_subnet" "test_public_a" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "test-public-a"
+  }
+}
+
+## Staging Bastion
+resource "aws_instance" "test_bastion" {
+  ami               = "ami-051dcca84f1edfff1"
+  instance_type     = "t2.micro"
+  subnet_id         = aws_subnet.test_public_a.id
+  availability_zone = "us-east-1a"
+}
+
