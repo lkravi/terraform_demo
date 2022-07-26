61d0ef7221b452219b914a1417315b32f277404d
Ravi Yasakeerthi <sliit.rnp@gmail.com>
2022-07-26 13:20:47 +1000

    changing staging vpc CIDR

--- demo_02/main-stag.tf
+++ demo_02/main-stag.tf
@@ -1,6 +1,6 @@
 ## Test VPC
 resource "aws_vpc" "test" {
-  cidr_block            = "10.0.0.0/16"
+  cidr_block            = "192.168.0.0/16"
   enable_dns_support    = true
   enable_dns_hostnames  = true 
 }
@@ -8,7 +8,7 @@ resource "aws_vpc" "test" {
 ## Test public Subnet
 resource "aws_subnet" "test_public_a" {
   vpc_id            = aws_vpc.test.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "192.168.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
