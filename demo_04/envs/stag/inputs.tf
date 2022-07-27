cf1f74733ed81ae563da97cb5077b23ac1cd3386
Ravi Yasakeerthi <sliit.rnp@gmail.com>
2022-07-27 21:30:12 +1000

    add Terraform presentation

--- demo_04/envs/stag/inputs.tf
+++ demo_04/envs/stag/inputs.tf
@@ -21,3 +21,14 @@ variable "ami_id" {
 variable "instance_type" {
     type = string
 }
+
+resource "aws_s3_bucket" "example" { }
+
+resource "aws_instance" "example_c" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  depends_on = [aws_s3_bucket.example]
+}
+
+
